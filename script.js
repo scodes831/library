3c8f33f9a8c557752339a0e11c1084d0770c58a8
scode234 <scodes3451@gmail.com>
2022-04-13 19:47:03 -0500

    Finished first version of library

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+const header = document.querySelector('header');
 const addButton = document.querySelector('.add-btn');
+const addContainer = document.querySelector('.add');
 const bookForm = document.querySelector('.book-form');
 const cancelBtn = document.getElementById('cancel-btn');
 const submitBtn = document.getElementById('submit-btn');
+const clearBtn = document.getElementById('clear-btn');
 const newWindow = document.querySelector('.window');
-
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+const hasReadInput = document.querySelector('input[name="read-status"]:checked');
+const bookShelf = document.getElementById('book-shelf');
 
 let bookLibrary = [];
-
-addToLibrary();
-closeForm();
 
 function Book(title, author, pages, hasRead) {
     this.title = title,
@@ -21,12 +25,21 @@ addButton.addEventListener('click', e => {
     const showForm = bookForm.style.display = "block";
     const newBackground = newWindow.style.display = "flex";
     const hideAddBtn = addButton.style.display = "none";
+    const hideAddContainer = addContainer.style.display = "none";
+    const hideBookShelf = bookShelf.style.display = "none";
 })
-submitBtn.addEventListener('click', addToLibrary)
-    //add user input to bookLibrary array - push method? when submit button clicked
-    //display newly added item on page
+submitBtn.addEventListener('click', addToLibrary);
+clearBtn.addEventListener('click', clearForm);
+cancelBtn.addEventListener('click', closeForm);
 
 function addToLibrary() {
+    //DON'T LET USER SUBMIT IF INPUTS ARE EMPTY
+    if (document.getElementById('title').value === "" || 
+        document.getElementById('author').value === "" || 
+        document.getElementById('pages').value === "") {
+            alert("You must enter the book information before submitting.")
+    } else {
+    //IF INPUTS ARE NOT EMPTY
     let book = {
         title: document.getElementById('title').value,
         author: document.getElementById('author').value,
@@ -35,25 +48,60 @@ function addToLibrary() {
     }
     bookLibrary.push(book);
     console.log({bookLibrary});
-    
-}
+    //DOM ELEMENTS
+    const bookEntry = document.createElement('div');
+    bookEntry.classList.add('book-entry');
+    bookShelf.appendChild(bookEntry);
+    const titleEntry = document.createElement('h2');
+    bookEntry.appendChild(titleEntry);
+    titleEntry.textContent = book.title 
+    const authorEntry = document.createElement('h3');
+    bookEntry.appendChild(authorEntry);
+    authorEntry.textContent = book.author;
+    const pagesEntry = document.createElement('h4');
+    bookEntry.appendChild(pagesEntry);
+    pagesEntry.textContent = `${book.pages} pages`;
+    const innerContainer = document.createElement('div');
+    innerContainer.classList.add('inner-container');
+    bookEntry.appendChild(innerContainer);
+    const readIcon = document.createElement('img');
+    if (document.querySelector('input[name="read-status"]:checked').value === "yes") {
+        readIcon.src = "/images/yes.png";
+        readIcon.classList.add('yes');
+    } else {
+        readIcon.src = "/images/no.png";
+        readIcon.classList.add('no');
+    }
+    innerContainer.appendChild(readIcon);
+    const removeIcon = document.createElement('img');
+    removeIcon.setAttribute('id', 'remove-icon');
+    removeIcon.src = "/images/remove.png";
+    innerContainer.appendChild(removeIcon);
+    //REMOVE BUTTON
+    removeIcon.addEventListener('click', e => {
+        removeIcon.parentElement.parentElement.remove();
+    });
+    //TOGGLE THE READ ICON
+    readIcon.addEventListener('click', e => {
+        if (readIcon.className === "yes") {
+            readIcon.src = "/images/no.png"
+        } else {
+            readIcon.src = "/images/yes.png"
+        }
+    })
+}}
 
-    function removeFromLibrary() {
-    //add event listener on remove button
-    //remove book from bookLibrary array
-    //book will no longer appear on screen
-}
-
-function updateReadStatus() {
-    //add event listner on has read/has not read button
-    //toggle value of object hasRead key on click
-    //book appears as 'has read' or 'has not read' on screen
+function clearForm() {
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
 }
 
 function closeForm() {
-    cancelBtn.addEventListener('click', e => {
-        const disableForm = bookForm.style.display = "none";
-        const originalBackground = newWindow.style.display = "none";
-        const showAddBtn = addButton.style.display = "block";
-    })
-};
+    const disableForm = bookForm.style.display = "none";
+    const originalBackground = newWindow.style.display = "none";
+    const showAddBtn = addButton.style.display = "block";
+    const showAddContainer = addContainer.style.display = "block";
+    const hideBookShelf = bookShelf.style.display = "grid";
+    clearForm();
+}
