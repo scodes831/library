0cb4bebafa44961321f7c2ea76a55e7349bf2bfc
scode234 <scodes3451@gmail.com>
2022-05-02 20:21:56 -0500

    finished refactoring and updated submit button to include clear function

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const addButton = document.querySelector('.add-btn');
 const addContainer = document.querySelector('.add');
 const bookForm = document.querySelector('.book-form');
 const cancelBtn = document.getElementById('cancel-btn');
-const submitBtn = document.getElementById('submit-btn');
-
 const newWindow = document.querySelector('.window');
 const bookShelf = document.getElementById('book-shelf');
 const footer = document.querySelector('footer');
@@ -17,20 +14,9 @@ class Book {
         this.pages = pages,
         this.hasRead = hasRead
     }
-
-    toggleRead() {
-        if (this.hasRead === "yes") {
-            this.hasRead === "no"
-        } else {
-            this.hasRead === "yes";
-        }
-    }
-
-    removeBook() {
-        //remove book from library display and arr
-    }
 }
 
+const addButton = document.querySelector('.add-btn');
 addButton.addEventListener('click', e => {
     const showForm = bookForm.style.display = "block";
     const newBackground = newWindow.style.display = "flex";
@@ -39,34 +25,71 @@ addButton.addEventListener('click', e => {
     const hideBookShelf = bookShelf.style.display = "none";
     const showFooter = footer.style.display = "block";
 })
-submitBtn.addEventListener('click', addToLibrary);
+
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', libraryControls);
 
 
-function addToLibrary() {
+function libraryControls() {
     const titleInput = document.getElementById('title').value;
     const authorInput = document.getElementById('author').value;
     const pagesInput = document.getElementById('pages').value;
     const hasReadInput = document.querySelector('input[name="read-status"]:checked').value;
-    const clearBtn = document.getElementById('clear-btn');
 
-    if (document.getElementById('title').value === "" || 
-        document.getElementById('author').value === "" || 
-        document.getElementById('pages').value === "") {
+    cancelBtn.addEventListener('click', closeForm);
+
+    if (titleInput === "" || authorInput === "" || pagesInput === "") {
             alert("You must enter the book information before submitting.")
     } else {
         let newBook = new Book(titleInput, authorInput, pagesInput, hasReadInput);
         bookLibrary.push(newBook);
         console.log({bookLibrary});
+        const bookEntry = document.createElement('div');
+        bookEntry.classList.add('book-entry');
+        bookShelf.appendChild(bookEntry);
+        const titleEntry = document.createElement('h2');
+        bookEntry.appendChild(titleEntry);
+        titleEntry.textContent = newBook.title 
+        const authorEntry = document.createElement('h3');
+        bookEntry.appendChild(authorEntry);
+        authorEntry.textContent = newBook.author;
+        const pagesEntry = document.createElement('h4');
+        bookEntry.appendChild(pagesEntry);
+        pagesEntry.textContent = `${newBook.pages} pages`;
+        const innerContainer = document.createElement('div');
+        innerContainer.classList.add('inner-container');
+        bookEntry.appendChild(innerContainer);
+        const readIcon = document.createElement('img');
+        if (document.querySelector('input[name="read-status"]:checked').value === "yes") {
+            readIcon.src = "/images/yes.png";
+            readIcon.classList.add('yes');
+        } else {
+            readIcon.src = "/images/no.png";
+            readIcon.classList.add('no');
+        }
+        innerContainer.appendChild(readIcon);
+        const removeIcon = document.createElement('img');
+        removeIcon.setAttribute('id', 'remove-icon');
+        removeIcon.src = "/images/remove.png";
+        innerContainer.appendChild(removeIcon);
+        removeIcon.addEventListener('click', e => {
+            removeIcon.parentElement.parentElement.remove();
+        });
+        readIcon.addEventListener('click', e => {
+            if (readIcon.className === "yes") {
+                readIcon.src = "/images/no.png"
+            } else {
+                readIcon.src = "/images/yes.png"
+            }
+        })
+        clearForm();
     }
-
-    clearBtn.addEventListener('click', clearForm);
-    cancelBtn.addEventListener('click', closeForm);
-
+    
     function clearForm() {
-        document.querySelector('.book-form').reset();
-        // titleInput = "";
-        // authorInput = "";
-        // pagesInput = "";
+        console.log('cleared');
+        document.getElementById('title').value = "";
+        document.getElementById('author').value = "";
+        document.getElementById('pages').value = "";
     }
 
     function closeForm() {
@@ -78,51 +101,11 @@ function addToLibrary() {
         const showFooter = footer.style.display = "none";
         clearForm();
     }
+}
+
 
     
-}
-//     //DOM ELEMENTS
-//     const bookEntry = document.createElement('div');
-//     bookEntry.classList.add('book-entry');
-//     bookShelf.appendChild(bookEntry);
-//     const titleEntry = document.createElement('h2');
-//     bookEntry.appendChild(titleEntry);
-//     titleEntry.textContent = book.title 
-//     const authorEntry = document.createElement('h3');
-//     bookEntry.appendChild(authorEntry);
-//     authorEntry.textContent = book.author;
-//     const pagesEntry = document.createElement('h4');
-//     bookEntry.appendChild(pagesEntry);
-//     pagesEntry.textContent = `${book.pages} pages`;
-//     const innerContainer = document.createElement('div');
-//     innerContainer.classList.add('inner-container');
-//     bookEntry.appendChild(innerContainer);
-//     const readIcon = document.createElement('img');
-//     if (document.querySelector('input[name="read-status"]:checked').value === "yes") {
-//         readIcon.src = "/images/yes.png";
-//         readIcon.classList.add('yes');
-//     } else {
-//         readIcon.src = "/images/no.png";
-//         readIcon.classList.add('no');
-//     }
-//     innerContainer.appendChild(readIcon);
-//     const removeIcon = document.createElement('img');
-//     removeIcon.setAttribute('id', 'remove-icon');
-//     removeIcon.src = "/images/remove.png";
-//     innerContainer.appendChild(removeIcon);
-//     //REMOVE BUTTON
-//     removeIcon.addEventListener('click', e => {
-//         removeIcon.parentElement.parentElement.remove();
-//     });
-//     //TOGGLE THE READ ICON
-//     readIcon.addEventListener('click', e => {
-//         if (readIcon.className === "yes") {
-//             readIcon.src = "/images/no.png"
-//         } else {
-//             readIcon.src = "/images/yes.png"
-//         }
-//     })
-// }}
+    
 
 
 
